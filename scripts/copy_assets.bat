0f0ebb696fdaa2ffe52d9a250e439f46dae17672
Al Arz <njotrvella@gmail.com>
2023-08-04 14:03:39 +0300

    fix build script

--- scripts/copy_assets.bat
+++ scripts/copy_assets.bat
@@ -1,15 +1,15 @@
 @echo off
 
 REM Remove dist/assets directory
-DEL /S "%CD%\dist\assets"
+DEL /S "%CD%\..\dist\assets"
 
 REM Remove index.html
-DEL /S "%CD%\dist\index.html"
+DEL /S "%CD%\..\dist\index.html"
 
-MKDIR "%CD%\dist\assets"
+MKDIR "%CD%\..\dist\assets"
 
 REM Copy assets
-xcopy /E /I /Y "%CD%\src\assets\" "%CD%\dist\assets"
+xcopy /E /I /H /Y "%CD%\..\src\assets\" "%CD%\..\dist\assets"
 
 REM Copy index.html
-xcopy /Y "%CD%\src\index.html" "%CD%\dist\index.html"
+xcopy /Y "%CD%\..\src\index.html" "%CD%\..\dist\index.html"
